0580b891e7c4ce7424888cc350c149dbfc570f1a
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-10 15:48:35 +0200

    page routing works

--- src/app/cities/page.jsx
+++ src/app/cities/page.jsx
@@ -1,5 +1,16 @@
-export default function Cities(){
-    return (<>
-        <h1>Cities</h1>
-        </>)
+'use client'
+
+import { useRouter } from "next/navigation";
+
+export default function Cities() {
+  const { back } = useRouter();
+  console.log(back);
+
+  return (
+    <>
+      <h1>Cities</h1>
+      <button onClick={back}>back</button>
+
+</>
+  );
 }
