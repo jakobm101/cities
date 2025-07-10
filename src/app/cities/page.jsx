803c8318298609c865f2ca487795551ffa9bfc3c
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-10 16:08:39 +0200

    created components City and Cities. functional

--- src/app/cities/page.jsx
+++ src/app/cities/page.jsx
@@ -1,25 +1,17 @@
 "use client";
+import City from "@/components/Cities";
 import cities from "../../../lib/data";
 import { useRouter } from "next/navigation";
 
 export default function Cities() {
   const { back } = useRouter();
-  console.log(cities);
 
   return (
     <>
       <button onClick={back}>back</button>
       <h1>Cities</h1>
-      {cities.map(({ id, name, description, country, population }) => (
-        <article key={id}>
-          <h2>{name}</h2>
-          <ul>
-            <li>{description}</li>
-            <li>{country}</li>
-            <li>{population}</li>
-          </ul>
-        </article>
-      ))}
+      <City cities={cities}/>
+      
     </>
   );
 }
