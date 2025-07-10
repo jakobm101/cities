1daef8e6952ca5c7c3f88c20bbeafba129fa3f6b
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-10 15:57:05 +0200

    mapping all the city datas

--- src/app/cities/page.jsx
+++ src/app/cities/page.jsx
@@ -1,16 +1,25 @@
-'use client'
-
+"use client";
+import cities from "../../../lib/data";
 import { useRouter } from "next/navigation";
 
 export default function Cities() {
   const { back } = useRouter();
-  console.log(back);
+  console.log(cities);
 
   return (
     <>
-      <h1>Cities</h1>
       <button onClick={back}>back</button>
-
-</>
+      <h1>Cities</h1>
+      {cities.map(({ id, name, description, country, population }) => (
+        <article key={id}>
+          <h2>{name}</h2>
+          <ul>
+            <li>{description}</li>
+            <li>{country}</li>
+            <li>{population}</li>
+          </ul>
+        </article>
+      ))}
+    </>
   );
 }
