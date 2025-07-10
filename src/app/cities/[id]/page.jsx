ac4216b0b5e0b97917afe64f021c612d2e1ef320
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-10 16:56:29 +0200

    done

--- src/app/cities/[id]/page.jsx
+++ src/app/cities/[id]/page.jsx
@@ -1,16 +1,19 @@
 import { use } from "react"
 import cities from "../../../../lib/data"
 import { Iceberg } from "next/font/google";
+import City from "@/components/Cities/City";
 
 export default function CityPage({params}) {
     const {id} = use(params)
-    const city = cities.find(item => item.id === 1)
+    // just "==" to avoid type mismatch
+    const city = cities.find(item => item.id == id || item.slug == id)
     console.log('⭐️',city);
     
     
     return (
         <>
-        <h1>Detail {id}</h1>
+        <h1>{city.name}</h1>
+        <City city={city}/>
         </>
     )
 }
